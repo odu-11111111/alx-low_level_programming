886130867adaec578e26ae002bac6c2448d0260b
Olayinka Onawola <olayinkaonawola@gmail.com>
2022-03-12 21:40:14 -0800

    comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-/*
- * @brief Function to try puts()
- * This is a function to print a string to the standard output.
- * @return 0
-*/
-
-int main(void)
+int main()
 {
+	/*
+	 * @brief Function to try puts()
+	 * This is a function to print a string to the standard output.
+	 * @return 0
+	*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
