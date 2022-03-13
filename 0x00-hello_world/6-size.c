ff38c733fcbc2c28c962739a45881eab3c096d85
Olayinka Onawola <olayinkaonawola@gmail.com>
2022-03-13 01:56:26 -0800

    ghp_

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %c  byte(s)", sizeof(char));
-	printf("Size of an int: %d byte(s)", sizeof(int));
-	printf("Size of a long int: %ld byte(s)", sizeof(long int));
-	printf("Size of a long long int: %lld byte(s)", sizeof(long long int));
-	printf("Size of a float: %f byte(s)", sizeof(float));
+	printf("Size of a char: %c  byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %f byte(s)\n", sizeof(float));
 	return (0);
 }
