88cba2b50ebe60d710284371f896b0c7ea70c514
Olayinka Onawola <olayinkaonawola@gmail.com>
2022-03-14 16:49:58 -0700

    commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function to putchar lowercase alphabets
+ * Return: zero
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; ++c)
+		putchar(c);
+		if (c != 57)
+			putchar(44);
+		putchar(32);
+	putchar('\n');
+	return (0);
+}
