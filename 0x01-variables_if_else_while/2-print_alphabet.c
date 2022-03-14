d3e172beaf1fefa7fe714d152b92092653ae080d
Olayinka Onawola <olayinkaonawola@gmail.com>
2022-03-14 15:07:33 -0700

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,10 +4,9 @@
  * main - function to putchar lowercase alphabets
  * Return: zero
  */
-int main(void) 
+int main(void)
 {
 	char c;
-
 	for (c = 'a'; c <= 'z'; ++c)
 		putchar(c);
 	putchar('\n');
