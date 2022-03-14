b4c6a62d1f6431bb52e0fed45edbbaa580bd16dc
Olayinka Onawola <olayinkaonawola@gmail.com>
2022-03-14 15:13:12 -0700

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function to putchar lowercase alphabets
+ * Return: zero
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; ++c)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
