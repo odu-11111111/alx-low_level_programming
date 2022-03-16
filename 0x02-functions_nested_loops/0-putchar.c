ba5aed43b51c3aff933ef6528a2e50a66df27f44
Olayinka Onawola <olayinkaonawola@gmail.com>
2022-03-16 05:31:29 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+int _putchar(char c);
+
+/*
+ * main -  a program that prints _putchar, followed by a new line.
+ * Return: 0
+ */
+int main()
+{
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar(10);
+	return (0);
+}
