d588d7eacfb844de1fddf21b72bef29241892a25
Olayinka Onawola <olayinkaonawola@gmail.com>
2022-03-17 09:41:07 -0700

    3-islower.c innitial commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a character is uppercase or lowercase
+ * Return: 1 if lowercase, 0 if otherwise
+ * @c: the number to check
+ */
+int _islower(int c)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; ++a)
+	{
+		if (a == c)
+			return 1;
+	}
+	return 0;
+}
