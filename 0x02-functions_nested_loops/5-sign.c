e9ee09c0e3557b4ac156e646c163056d49a989fd
Olayinka Onawola <olayinkaonawola@gmail.com>
2022-03-17 10:17:47 -0700

    5-sign.c v002

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _isalpha - checks if a character is uppercase or lowercase
+ * print_sign - prints approriate sign to indicat if int is positive, 0 or negative
+ * @n - number to check
  * Return: 1 if n > 0,, 0 if n is 0 and -1 if otherwise
  */
 int print_sign(int n)
