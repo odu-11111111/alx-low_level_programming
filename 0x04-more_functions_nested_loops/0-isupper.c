1db805c5c4c6c84fbe036bf458d31c0cba89b6ae
Olayinka Onawola <OlayinkaOnawola@gmail.com>
2022-03-20 19:12:07 +0000

    Happy first commit from my local vagrant machine.

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_isupper - function that checks for uppercase character.
+ *@i: increment variable.
+ *@c: char variable to check
+ *Return: 1 if c is uppercase, 0 otherwise
+ */
+
+
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 65; i <= 90; i++)
+		if (c == i)
+			return 1;
+	return 0;
+}
