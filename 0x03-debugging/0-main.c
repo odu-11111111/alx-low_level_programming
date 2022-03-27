7a2fd6a229b9052eb18abbea79707c2baa434d39
Olayinka Onawola <OlayinkaOnawola@gmail.com>
2022-03-27 22:25:12 +0000

    positive_or_negative.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,26 +1,16 @@
-#include <stdlib.h>
 #include "main.h"
-#include <stdio.h>
-
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
 
 /**
- *  * main - function to printf random number
- *   * Return: return 0
- *    */
+ ** main - tests function that prints if integer is positive or negative
+ ** Return: 0
+ **/
+
 int main(void)
 {
-        int n = 0;
-	
-	postive_or_negative(n);
-	/* your code goes there */
-	if (n < 0)
-		printf("%d is negative\n", n);
-	else if (n > 0)
-		printf("%d is positive\n", n);
-	else
-		printf("%d is zero\n", n);
-	return (0);
+        int i;
+
+        i = 48;
+        positive_or_negative(i);
+
+        return (0);
 }
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+THis file contains my debugging project while at ALX cohort 5
