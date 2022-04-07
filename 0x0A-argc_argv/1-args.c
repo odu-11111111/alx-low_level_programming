443144ef04af39e273c19666445c3a0a7a5fd8d1
Olayinka Onawola <OlayinkaOnawola@gmail.com>
2022-04-07 17:45:49 +0000

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ * main -  program that prints the number of arguments passed into it
+ * @argc: count of command line arguments
+ * @argv: pointer to array of pointer pointing to argument vector
+ * Return: 0
+ */
+
+int main(int argc,__attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
