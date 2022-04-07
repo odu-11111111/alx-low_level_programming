23118c3f0dacb5dda8c1858163cd8dec8664a998
Olayinka Onawola <OlayinkaOnawola@gmail.com>
2022-04-07 17:36:27 +0000

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <limits.h>
 
+/**
+ * main - program that prints its name, followed by a new line.
+ * @argc: count of command line arguments
+ * @argv: pointer to array of pointer pointing to argument vector
+ * Return: 0
+ */
+
 int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
